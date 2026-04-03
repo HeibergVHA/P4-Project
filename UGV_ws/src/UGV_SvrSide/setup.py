7542dd263c5a1cf14f53f5acaddb816925f705a2
HeibergVHA <heibergvha@gmail.com>
2026-04-03 14:35:55 +0200

    Hej

---
+++ UGV_ws/src/UGV_SvrSide/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'PLC'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/PLC/', ['PLC/procssing_times_table.csv']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='heiberg',
+    maintainer_email='heibergvha@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'tcp_node = PLC.Tcp_node:main',
+        'process_node = PLC.process_node:main',
+        ],
+    },
+)
+
