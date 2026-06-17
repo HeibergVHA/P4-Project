2b33eb570a13886825e7600d150de032576a5751
HeibergVHA <heibergvha@gmail.com>
2026-06-17 18:22:27 +0200

    Added log line

--- UGV_ws/src/UGV_SvrSide/UGV_SvrSide/ugv_controller_node.py
+++ UGV_ws/src/UGV_SvrSide/UGV_SvrSide/ugv_controller_node.py
@@ -196,6 +196,7 @@ class UGVController(Node):
                     speed_val = int(np.clip(self.speed_val*100 , 0, 200))
                     steer_bytes = f"{steer_val},{speed_val}\n".encode("utf-8")
                     self.ser.write(steer_bytes)
+                    self.get_logger().info(f"Sent to serial: {steer_bytes}")
 
 
                 # Read
